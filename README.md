511d84a2a064da871b1ef40eaf61864c506b8601
rohitmore02 <rohitmore386@gmail.com>
2023-01-13 22:31:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Movie App built in React JS and Material UI.
+
+Here you will get Top/Trending Movies as well as Series, Rating & other Details regarding Movies.
---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,82 @@
+import { Chip } from "@material-ui/core";
+import axios from "axios";
+import { useEffect } from "react";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const Genres = ({
+  selectedGenres,
+  setSelectedGenres,
+  genres,
+  setGenres,
+  type,
+  setPage,
+}) => {
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    setGenres(data.genres);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+
+    return () => {
+      setGenres({}); // unmounting
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {selectedGenres.map((genre) => (
+        <ThemeProvider theme={darkTheme}>
+          <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+        </ThemeProvider>
+      ))}
+      {genres.map((genre) => (
+        <ThemeProvider theme={darkTheme} >
+          <Chip 
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          clickable
+          size="small"
+          variant="outlined"
+          onClick={() => handleAdd(genre)}
+        />
+        </ThemeProvider>
+      ))}
+    </div>
+  );
+};
+
+export default Genres;
